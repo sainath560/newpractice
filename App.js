60317c8b40a035c882eab09ea93f9630f3f2d2a7
sainath560 <sainathbeeram@gmail.com>
2024-05-12 19:14:30 +0530

    myfirst

---
+++ App.js
@@ -0,0 +1,10 @@
+
+const parent=React.createElement("div",{id:'parent'},React.createElement("div",{
+    id:'child'
+}, React.createElement("h1",{},"hi bro how are you")))
+
+
+    
+        const xxx=ReactDOM.createRoot(document.getElementById('header'));
+        xxx.render(parent);
+    
